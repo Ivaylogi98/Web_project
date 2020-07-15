6bea324b3b86c43e71704da13273b156d1e851ab
Ivaylo Ivanov <ivaylogi98@gmail.com>
2020-07-15 16:31:31 +0300

    Changed Json responce and minor changes to midi-parser

--- midi-parser.php
+++ midi-parser.php
@@ -69,12 +69,10 @@ class Midi{
             //delta time = how much time has to pass after the previous event for the current event to take place
             $dt = readVarLen($binStr,$p);
             $time += $dt;
-        
             $byte = ord($binStr[$p]);
             $high = $byte >> 4;
             $low = $byte - $high*16;
             $chan = $low+1;
-            $last = 'NotSet';
             switch($high){
                 case 0x0C: //PrCh = ProgramChange
                     $chan = $low+1;
@@ -229,8 +227,7 @@ class Midi{
                                     $p+=$len+3;
                                     break;
                             } // switch ($meta)
-                            break; // Ende Meta
-        
+                            break; // End Meta
                         case 0xF0: // SysEx
                             $p +=1;
                             $len = readVarLen($binStr, $p);
@@ -240,46 +237,17 @@ class Midi{
                             $track[] = "$time SysEx $str";
                             $p+=$len;
                             break;
+                        case 0xF7:  // DivSysEx
+                            $p +=1;
+                            $len = readVarLen($binStr, $p);
+                            if (($len+$p) > $trackLen) error("DivSysEx has corrupt variable length field ($len) [track: $tn dt: $dt p: $p]");
+                            $str = 'f0';
+                            for ($i=0;$i<$len;$i++) $str.=' '.sprintf("%02x",ord($binStr[$p+$i])); # FIXED
+                            $track[] = "$time DivSysEx $str";
+                            $p+=$len;
+                            break;
                         default: // Repetition of last event?
-                            switch ($last){
-                                case 'On':
-                                case 'Off':
-                                    $note = ord($binStr[$p]);
-                                    $vel = ord($binStr[$p+1]);
-                                    $track[] = "$time $last ch=$chan n=$note v=$vel";
-                                    $p+=2;
-                                    break;
-                                case 'PrCh':
-                                    $prog = ord($binStr[$p]);
-                                    $track[] = "$time PrCh ch=$chan p=$prog";
-                                    $p+=1;
-                                    break;
-                                case 'PoPr':
-                                    $note = ord($binStr[$p+1]);
-                                    $val = ord($binStr[$p+2]);
-                                    $track[] = "$time PoPr ch=$chan n=$note v=$val";
-                                    $p+=2;
-                                    break;
-                                case 'ChPr':
-                                    $val = ord($binStr[$p]);
-                                    $track[] = "$time ChPr ch=$chan v=$val";
-                                    $p+=1;
-                                    break;
-                                case 'Par':
-                                    $c = ord($binStr[$p]);
-                                    $val = ord($binStr[$p+1]);
-                                    $track[] = "$time Par ch=$chan c=$c v=$val";
-                                    $p+=2;
-                                    break;
-                                case 'Pb':
-                                    $val = (ord($binStr[$p])  & 0x7F) | (( ord($binStr[$p+1]) & 0x7F)<<7);
-                                    $track[] = "$time Pb ch=$chan v=$val";
-                                    $p+=2;
-                                    break;
-                                default:
-            // MM: ToDo: Repetition of SysEx and META-events? with <last>?? \n";
-                                    error("unknown repetition: $last");
-                            }  // switch ($last)
+                            error("unknown event: $byte");
                     } // switch ($byte)
             } // switch ($high)
         } // while
@@ -287,8 +255,10 @@ class Midi{
     }
 
     function midi2json(){
-        $notes = array();
+        $song = array();
+        $track_num = 1;
         foreach($this->tracks as $track){
+            $json_track = array();
             $track_length = count($track);
             $track_end_time = explode(' ',$track[$track_length - 2])[0];
             for($i = 0; $i < $track_length; $i++)
@@ -305,14 +275,21 @@ class Midi{
                         }
                     }
                     $note_int = explode('=',$event[3])[1];
-                    $notes_str = ["c3", "c_3", "d3", "d_3", "e3", "f3", "f_3", "g3", "a3", "a_3", "b3", "c4", "c_4", "d4", "d_4", "e4", "f4", "f_4", "g4", "a4", "a_4", "b4"];
-                    
-                    $note = $notes_str[$note_int - 48].' '.$event[0].' '.$end_note;
-                    array_push($notes, $note);
+                    $note_names = ["c", "c_", "d", "d_", "e", "f", "f_", "g", "g_", "a", "a_", "b"];
+                    $note_name = $note_names[($note_int-24) % 12].floor(($note_int-12)/12);
+                    $json_note = array("note"=>$note_name, "start"=>$event[0], "end"=>$end_note);
+                    array_push($json_track, $json_note);
                 }
             }
+            if(count($json_track) > 0){
+                array_push($song, array("track".$track_num => $json_track));
+                $track_num++;
+            }
         }
-        echo json_encode($notes);
+        $fp = fopen('example.json', 'w');
+        fwrite($fp, json_encode($song));
+        fclose($fp);
+        echo json_encode($song);
     }
 
 }
@@ -327,6 +304,8 @@ function readVarLen($str,&$pos){
     return($value);
 }
 function error($str){
+    echo "Error: ".$str;
+    exit();
     if ((int)phpversion()>=5)
 	eval('throw new Exception($str);'); // throws php5-exceptions. the main script can deal with these errors.
 	else
