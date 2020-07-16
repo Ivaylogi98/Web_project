da3dd00f573f033a56aab25a4b8b366ecfd5deac
Ivaylo Ivanov <ivaylogi98@gmail.com>
2020-07-16 16:38:39 +0300

    Worked on note editor

--- editor.js
+++ editor.js
@@ -1,6 +1,12 @@
 
 document.getElementById("import").addEventListener("change", importFile);
 document.getElementById("export").addEventListener("click", exportFile);
+document.getElementById("octave").addEventListener("change", changeOctave());
+
+var song;
+var numberOfTracks = 0;
+var chosenTrack = 1;
+var chosenOctave = 1;
 
 async function fetchData(url, dataToSend) {
     try {
@@ -8,7 +14,7 @@ async function fetchData(url, dataToSend) {
             method: "post",
             body: dataToSend
         });
-        return response.text();
+        return response.json();
     } catch (error) {
         console.error(error);
     }
@@ -25,8 +31,11 @@ async function importFile(event) {
     dataToSend.append('file', file);
     var content = await fetchData(url, dataToSend);
     document.getElementById("timeline").innerHTML = content;
-
+    song = content;
+    console.log(song);
     console.log("imported " + file.name);
+
+    loadTimeline(song);
 }
 
 function displayContents(contents) {
@@ -54,4 +63,45 @@ function exportFile(content) {
     }
 
     console.log("exporting " + filename);
+}
+
+function addTrack(){
+
+    var track = document.createElement("section");
+    track.setAttribute("id", "track" + (numberOfTracks + 1));
+    track.setAttribute("class", "tracks");
+    document.getElementById("timeline").appendChild(track);
+}
+
+function addNote(noteTextName){
+    console.log("adding note:" + note);
+    var note = document.createElement("li");
+    note.setAttribute("class", "note");
+
+    var noteName = document.createTextNode(noteTextName);
+    noteName.innerHTML = "Choose note";
+    note.appendChild(noteName);
+
+    var noteStart = document.createElement("input");
+    noteStart.setAttribute("type", "number");
+    noteStart.setAttribute("class", "note-attribute");
+    note.appendChild(noteStart);
+
+    var noteEnd = document.createElement("input");
+    noteEnd.setAttribute("type", "number");
+    noteEnd.setAttribute("class", "note-attribute");
+    note.appendChild(noteEnd);
+
+    document.getElementById("track" + chosenTrack).appendChild(note);
+}
+function showNotes(){
+    var x = document.getElementById("note-tools");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+}
+function changeOctave(){
+    chosenOctave = document.getElementById("octave").value;
 }
--- editor.php
+++ editor.php
@@ -5,14 +5,30 @@ require("./header.php");
 <script async src="editor.js"></script>
 
 <div class="tools">
-    <button class="tool">Add note</button>
-    <button class="tool">Add space</button>
-    <button class="tool">Set tempo</button>
+    <button class="tool" id="add-track" onclick="addTrack()">Add track</button>
+    <button class="tool" id="add-note" onclick="showNotes()">Show notes</button>
     <label for="import" class="import-tool">Import</label>
     <input type="file" id="import"></input>
     <button type="button" class="tool" id="export">Export</button>
 </div>
-<div id="timeline" contenteditable="true">Timeline</div>
+<div class="tools" id="note-tools">
+    <label for="octave" class="note-tool">Octave:</label>
+    <input type="number" id="octave" min=0 max=7 value="1">
+    <label class="note-tool">Add:</label>
+    <button class="note-name-button" id="add-track" onclick="addNote('c')">C</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('c_')">C#</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('d')">D</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('d_')">D#</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('e')">E</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('f')">F</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('f_')">F#</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('g')">G</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('g_')">G#</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('a')">A</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('a_')">A#</button>
+    <button class="note-name-button" id="add-track" onclick="addNote('b')">B</button>
+</div>
+<ul id="timeline"></ul>
 
 <?php
 require("./footer.php");
